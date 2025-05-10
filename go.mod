6abeb7f3f3f270ebb2fea3599c2e198eae8a8ddb
Andy Kreling <krelinga@gmail.com>
2025-05-10 01:39:43 +0000

    working demo for looking up movie details.

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/krelinga/go-tmdb-playground
 
 go 1.23.8
+
+require github.com/ryanbradynd05/go-tmdb v0.0.0-20230108222638-2a68dc6ff40c
+
+require (
+	github.com/kylelemons/go-gypsy v1.0.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/ryanbradynd05/go-tmdb"
+)
+
+func newClient() (*tmdb.TMDb, error) {
+	apiKey, err := readApiKey()
+	if err != nil {
+		return nil, err
+	}
+
+	config := tmdb.Config{
+		APIKey: apiKey,
+	}
+	return tmdb.Init(config), nil
+}
+
+func demo() error {
+	client, err := newClient()
+	if err != nil {
+		return err
+	}
+
+	movieID := 550 // Example movie ID for "Fight Club"
+	movie, err := client.GetMovieInfo(movieID, nil)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(movie)
+
+	return nil
+}
 
 func main() {
-	// This is a simple Go program that prints "Hello, World!" to the console.
-	fmt.Println("Hello, World!")
+	if err := demo(); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 }
